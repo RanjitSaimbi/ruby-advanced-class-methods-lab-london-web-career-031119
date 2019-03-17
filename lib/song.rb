757a06b8e1e199d3c84ddcf019fa2b9e06e40068
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-17 11:58:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,7 @@ class Song
     file_name_array = file_name.sub(/\.mp3/,"").split(" - ")
     new_song = Song.new
     binding.pry 
+    "ghdg"
     new_song.name = file_name_array[1]
     new_song.artist_name = file_name_array[0]
   end 

249d52e6a998d812494b4ff0673d681689a5419e
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-17 11:57:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
     file_name_array = file_name.sub(/\.mp3/,"").split(" - ")
     new_song = Song.new
     new_song.name = file_name_array[1].join("")
-    new_song.artist_name = file_name_array[0]
+    new_song.artist_name = file_name_array[0].join("")
   end 
 
 end
